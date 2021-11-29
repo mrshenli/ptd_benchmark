8b17d03d7a5020a7536d063f5e1b345a8bdf36f1
Shen Li <cs.shenli@gmail.com>
2021-11-29 07:55:51 -0800

    use hostname instead of ip

--- launch_4_node_ddp.sh
+++ launch_4_node_ddp.sh
@@ -4,4 +4,4 @@ source /home/shenli_fb_com/.bashrc
 
 ulimit -S -c unlimited
 
-torchrun --nnodes=4 --nproc_per_node=8 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint="10.128.0.38:5678" trainer.py
+torchrun --nnodes=4 --nproc_per_node=8 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint="shen-64-a2-highgpu-8g-compute-0-0:5678" trainer.py
