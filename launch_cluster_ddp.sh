e2080718aeacce1a239ec73b1563f7238f6a8f42
mrshenli <shenli@fb.com>
2021-11-30 02:50:00 +0000

    rename ddp launcher script

---
+++ launch_cluster_ddp.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+source /home/shenli_fb_com/.bashrc
+
+ulimit -S -c unlimited
+
+torchrun --nnodes=64 --nproc_per_node=8 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint="shen-64-a2-highgpu-8g-compute-0-0:5678" trainer.py
