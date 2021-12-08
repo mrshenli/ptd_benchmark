3924416c6c44fcb0195cdc1aa0d3724bf6dd1f99
mrshenli <shenli@fb.com>
2021-12-08 04:50:45 +0000

    add launching script

--- multi_launch_cluster_fsdp.sh
+++ multi_launch_cluster_fsdp.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-srun --label launch_cluster_fsdp.sh 16
+srun --label launch_cluster_fsdp.sh 8 checkpoint
 
-srun --label sbatch_cluster_fsdp.sh 24
+srun --label launch_cluster_fsdp.sh 16 checkpoint
+
+srun --label launch_cluster_fsdp.sh 24 checkpoint
+
+srun --label launch_cluster_fsdp.sh 32 checkpoint
+
+srun --label launch_cluster_fsdp.sh 40 checkpoint
+
+srun --label launch_cluster_fsdp.sh 48 checkpoint
+
+srun --label launch_cluster_fsdp.sh 56 checkpoint
+
+srun --label launch_cluster_fsdp.sh 64 checkpoint
+
+srun --label launch_cluster_fsdp.sh 32 offload
+
+srun --label launch_cluster_fsdp.sh 40 offload
+
+srun --label launch_cluster_fsdp.sh 48 offload
+
+srun --label launch_cluster_fsdp.sh 56 offload
+
+srun --label launch_cluster_fsdp.sh 64 offload
